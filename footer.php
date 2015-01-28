f889df8ae86d3f4f666ecdde2afcd440c2f64738
Robciuc Ioan <robciucioan94@gmail.com>
2015-01-28 07:48:38 +0200

    Issue #52 fixed (New copyright)
    Copyright (C) [Nume site] | Proudly powered by WordPress.

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 		<footer>
 			<div class="wrap">
 				<div class="footer-copyright">
-					<?php _e( 'Copyright &copy;', 'denta_lite' ); ?> <a href="<?php echo esc_url( home_url() ); ?>" title="<?php _e( 'Denta Lite', 'denta_lite' ); ?>"><?php _e( 'Denta Lite', 'denta_lite' ); ?></a> <?php _e( 'is proudly powered by', 'denta_lite' ); ?> <a href="http://www.wordpress.org" title="<?php _e( 'WordPress', 'denta_lite' ); ?>" target="_blank"><?php _e( 'WordPress', 'denta_lite' ); ?></a>.
+					<?php _e( 'Copyright &copy;', 'denta_lite' ); ?> <a href="<?php echo esc_url( home_url() ); ?>" title="<?php bloginfo( 'name' ); ?>"><?php bloginfo( 'name' ); ?></a> <?php _e( '| Proudly powered by', 'denta_lite' ); ?> <a href="http://www.wordpress.org" title="<?php _e( 'WordPress', 'denta_lite' ); ?>" target="_blank"><?php _e( 'WordPress', 'denta_lite' ); ?></a><?php _e( '.', 'denta_lite' ); ?>
 				</div><!--/.footer-copyright-->
 			</div><!--/.wrap-->
 		</footer>
