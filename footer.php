892e903b94a143b676f56a3fa417728a467810f1
Robciuc Ioan <robciucioan94@gmail.com>
2015-02-12 13:46:20 +0200

    Issue #59 (Copyright issue) fixed

--- footer.php
+++ footer.php
@@ -58,7 +58,7 @@
 					<?php echo get_theme_mod( 'denta_lite_footer_entry', 'Copyright &copy; ThemeIsle.' ); ?>
 				</div><!--/.footer-left-->
 				<div class="footer-right">
-					<a href="<?php echo esc_url( home_url() ); ?>" title="<?php bloginfo( 'name' ); ?>" rel="nofollow"><?php bloginfo( 'name' ); ?></a> | Proudly powered by <a href="http://www.wordpress.org" title="WordPress" target="_blank" rel="nofollow">WordPress</a>.
+					<a href="<?php echo esc_url( home_url() ); ?>" title="<?php bloginfo( 'name' ); ?>" rel="nofollow"><?php bloginfo( 'name' ); ?></a> <?php _e( '| Proudly powered by', 'denta_lite' ); ?> <a href="http://www.wordpress.org" title="<?php _e( 'WordPress', 'denta_lite' ); ?>" target="_blank" rel="nofollow"><?php _e( 'WordPress', 'denta_lite' ); ?></a>.
 				</div><!--/.footer-right-->
 			</div><!--/.wrap.cf-->
 		</footer>
